7bc589abe94c87e46e6224f96967bd460690268c
Jakub Szopa <jkbszopa@gmail.com>
2022-03-16 15:01:25 +0100

    Still a rough idea of the page

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 var myCarousel = document.querySelector('#myCarousel');
 var carousel = new bootstrap.Carousel(myCarousel);
+
+const menuButton = document.querySelector('#nav-bar-icon button');
+const BigMenu = document.getElementById('Menu2');
+
+function {
+    BigMenu.style.display = 'block'
+}
